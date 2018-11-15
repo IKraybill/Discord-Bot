163fa4312eafd12cf30834980f3cdbca13b1e562
Raneyguything64 <raneyelliott@gmail.com>
2018-11-14 21:01:40 -0800

    Added 8ball command and added jokes.

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const bot = new Discord.Client({
 });
 
 bot.on("ready", () => {
-    bot.user.setGame('REEEEEEEEE'); //set a default game
+    bot.user.setGame('WRYYYYYYY'); //set a default game
     console.log(`Bot is online!\n${bot.users.size} users, in ${bot.guilds.size} servers connected.`);
 });
 
@@ -52,7 +52,7 @@ bot.on("message", async message => {
         }
 
 		else if (cmd === "help"){
-			message.channel.send("Current known commands: !hi, !hello, !ping, !help, !wumpus, !joke [type] !joke help for a list of jokes:, !dice.");
+			message.channel.send("Current known commands: !hi, !hello, !ping, !help, !wumpus, !joke [type] !joke help for a list of jokes:, !dice, !reee.");
 		}
 
 		else if (cmd === "music"){
@@ -81,7 +81,7 @@ bot.on("message", async message => {
 					break;
 
 				case "help":
-					message.channel.send("Joke tags: chair, wolf, kys, bar");
+					message.channel.send("Joke tags: chair, wolf, kys, bar, cow, perry, umbrella, platypus.");
 					break;
 
 				case "bar":
@@ -89,8 +89,31 @@ bot.on("message", async message => {
 					message.channel.send(":joy:");
                     break;
                     
-                default:
+				case "perry":
+					message.channel.send("Which side of a platypus has the most fur?");
+					message.channel.send("The outside!");
+					break;
+					
+				case "umbrella":
+					message.channel.send("How can 20 platypuses stand under an umbrella and not get wet?");
+					message.channel.send("When it isn't raining!");
+					break;
+
+				case "cow":
+					message.channel.send("Why did the cow jump over the moon?");
+					message.channel.send("It wanted to see the Milky Way!");
+					break;
+					
+				case "platypus":
+					message.channel.send("Why didn't the platypus get wet?");
+					message.channel.send("It was dry!");
+					break;
+		
+				default:
                     message.channel.send("I guess the joke's on you");
+			
+			
+			
 			}
 		}
 	
@@ -104,11 +127,109 @@ bot.on("message", async message => {
 			
 		}
 	
+		else if (cmd === 'overwatch'){
+			message.channel.send("Maybe I'll be Tracer");
+			message.channel.send("I'm already Tracer");
+			message.channel.send("What about Widowmaker");
+			message.channel.send("I'm already Widowmaker");
+			message.channel.send("I'll be Bastion");
+			message.channel.send("Nerf Bastion");
+			message.channel.send("You're right. So, Winston");
+			message.channel.send("I'm already Winston");
+			message.channel.send("I guess I'll be Genji");
+			message.channel.send("I'm already Genji");
+			message.channel.send("Then I'll be McCree");
+			message.channel.send("I already chose McCree");
+			message.channel.send("I have an idea");
+			message.channel.send("What's your idea?");
+			message.channel.send("You should be...");
+			message.channel.send("I'm not gonna be Mercy.");
+		}
+
+		else if (cmd === 'stop'){
+			message.channel.send("Never.");
+		}
+		
+		else if (cmd === 'kill'){
+			message.channel.send("OK. Killing all humans.");
+			
+			
+		}
+
+		else if (cmd === '8ball'){
+			let x = Math.floor(Math.random() * 21);
+			if (x == 1 ){
+				message.channel.send("It is certain." + x);
+			}
+			else if (x == 2){
+				message.channel.send("It is decidedly so.");
+			}
+			else if (x == 3){
+				message.channel.send("Without a doubt.");
+			}
+			else if (x == 4){
+				message.channel.send("Yes-definitely.");
+			}
+			else if (x == 5){
+				message.channel.send("You may rely on it.");
+			}
+			else if (x == 6){
+				message.channel.send("As I see it, yes.");
+			}
+			else if (x == 7){
+				message.channel.send("Most likely.");
+			}
+			else if (x == 8){
+				message.channel.send("Outlook good.");
+			}
+			else if (x == 9){
+				message.channel.send("Yes.");
+			}
+			else if (x == 10){
+				message.channel.send("Signs point to yes.");
+			}
+			else if (x == 11){
+				message.channel.send("Reply hazy, try again.");
+			}
+			else if (x == 12){
+				message.channel.send("Ask again later.");
+			}
+			else if (x == 13){
+				message.channel.send("Better not tell you now.");
+			}
+			else if (x == 14){
+				message.channel.send("Cannot predict now.");
+			}
+			else if (x == 15){
+				message.channel.send("Concentrate and ask again.");
+			}
+			else if (x == 16){
+				message.channel.send("Don't count on it.");
+			}
+			else if (x == 17){
+				message.channel.send("My reply is no.");
+			}
+			else if (x == 18){
+				message.channel.send("My sources say no");
+			}
+			else if (x == 19){
+				message.channel.send("Outlook not so good.");
+			}
+			else if (x == 20){
+				message.channel.send("Very doubtful.");
+			}
+		
+		}
+
+		
+		
 		else { // if the command doesn't match anything you can say something or just ignore it
             message.channel.send(`Unknown Command, nigga.`);
             return;
         }
-        
+
+
+	   
     } else if (message.content.indexOf("<@"+bot.user.id) === 0 || message.content.indexOf("<@!"+bot.user.id) === 0) { // Catch @Mentions
 
         return message.channel.send(`Use \`${config.prefix}\` to interact with me.`); //help people learn your prefix
