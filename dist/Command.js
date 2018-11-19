ebde5f132ee90a3463a44554996ddb5db3ff3905
Ian Kraybill <iek2day@gmail.com>
2018-11-18 17:32:22 -0800

    Added argument help system and extra help info for quotes

--- dist/Command.js
+++ dist/Command.js
@@ -6,10 +6,12 @@ class Command {
      * @param name: name of the command; is what the user types in
      * @param task: function the command performs. has access to Discord messages, command arguments, and parent
      * command set
+     * @param argHelp
      */
-    constructor(name, task) {
+    constructor(name, task, argHelp = null) {
         this.name = name;
         this.task = task;
+        this.argHelp = argHelp;
     }
     task(message, args, parent) {
         message.channel.send("Error, no task specified");
