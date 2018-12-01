ce35224e2b8e58f49b14bb2e2819bb66db14359b
IKraybill <iek2day@gmail.com>
2018-12-01 01:45:43 +0000

    Begun Kotlin port

--- src/index.kt
+++ src/index.kt
@@ -1,4 +1,16 @@
+import kotlin.js.Json
+
+external fun require(module: String): dynamic
+
+
+
 fun main(args: Array<String>) {
     val message = "Hello JavaScript!"
+
+    //language=JavaScript
+    js("var discord = require('discord.js')")
+    //language=JavaScript
+    val bot = js("new discord.Client({ disableEveryone: true, disabledEvents: ['TYPING_START']})")
+
     println(message)
 }
