ed94aafbc0ade27682d212eccf787093dae03e81
Ian Kraybill <iek2day@gmail.com>
2018-12-23 12:16:10 -0800

    Remove process exceptions (temporarily?)

--- src/index.ts
+++ src/index.ts
@@ -39,24 +39,24 @@ bot.on("message", async message => {
     }
 });
 
-bot.on('disconnected', () => {
-    console.log("disconnected, logging in again");
-    bot.login(config.token)
-});
-
-//bot.on('error', console.error);
-
-// Catch Errors before they crash the app.
-process.on('uncaughtException', (err) => {
-    const errorMsg = err.stack.replace(new RegExp(`${__dirname}/`, 'g'), './');
-    console.error('Uncaught Exception: ', errorMsg);
-    // process.exit(1); //Eh, should be fine, but maybe handle this?
-    bot.login(config.token)
-});
-
-process.on('unhandledRejection', err => {
-    console.error('Uncaught Promise Error: ', err);
-    // process.exit(1); //Eh, should be fine, but maybe handle this?
-});
+// bot.on('disconnected', () => {
+//     console.log("disconnected, logging in again");
+//     bot.login(config.token)
+// });
+//
+// //bot.on('error', console.error);
+//
+// // Catch Errors before they crash the app.
+// process.on('uncaughtException', (err) => {
+//     const errorMsg = err.stack.replace(new RegExp(`${__dirname}/`, 'g'), './');
+//     console.error('Uncaught Exception: ', errorMsg);
+//     // process.exit(1); //Eh, should be fine, but maybe handle this?
+//     bot.login(config.token)
+// });
+//
+// process.on('unhandledRejection', err => {
+//     console.error('Uncaught Promise Error: ', err);
+//     // process.exit(1); //Eh, should be fine, but maybe handle this?
+// });
 
 bot.login(config.token);
